9ce3535686959f2362b698952f32080febcaff9c
xaviguasch <xaviguasch@gmail.com>
2019-05-13 23:50:49 +0200

    added a 404 not found route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import Food from './Food'
 import Meal from './Meal'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
       {/* <Route exact path='/food/:name' component={Food} /> */}
-      <Route
-        exact
-        path='/food/:name'
-        render={routeProps => <Food authenticated={true} {...routeProps} />}
-      />
-      <Route exact path='/food/:foodName/drink/:drinkName' component={Meal} />
+      <Switch>
+        <Route
+          exact
+          path='/food/:name'
+          render={routeProps => <Food authenticated={true} {...routeProps} />}
+        />
+        <Route exact path='/food/:foodName/drink/:drinkName' component={Meal} />
+        <Route exact path='/' render={() => <h1>Home page</h1>} />
+        <Route render={() => <h1>ERROR NOT FOUND</h1>} />
+      </Switch>
     </div>
   )
 }
